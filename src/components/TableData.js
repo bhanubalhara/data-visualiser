a126eb21cdb176e39c9ffe5042b016dd1262d6a2
bhanubalhara <159030072+bhanubalhara@users.noreply.github.com>
2024-02-06 23:15:20 +0530

    Update TableData.js

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,75 +1,43 @@
-import { useState, useEffect } from "react";
-import Header from "./Header";
-import fetchApiData from "../services/api";
-import PaginationComponent from "./Pagination";
-import RowData from "./RowData";
-import { Row, Table } from "react-bootstrap";
-import { fetchDataByCategory } from "../services/filterApi";
+import { Dropdown, Form } from 'react-bootstrap';
+import '../styles/header.css';
+import { useEffect, useState } from 'react';
+import { fetchAllCategories } from '../services/filterApi';
 
-function TableData({onDataSelected}){
-    const [data, setData] = useState([]);
-    const [selectedRows, setSelectedRows] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const [recordsPerPage] = useState(10);
+export default function Header({categoryFilter, categoryHandler}){
+    const [categories, setCategories] = useState([]);
 
     const fetchData = () => {
-        fetchApiData((currentPage-1)*recordsPerPage,recordsPerPage).then((res) => {
-            if(res.status === 200 && res.data !== undefined && res.data.products !== undefined){
-                setData(res.data.products);
-                setTotalPages(res.data.total/recordsPerPage);
-                setCurrentPage(res.data.skip/recordsPerPage+1);
+        fetchAllCategories().then((res) => {
+            if(res.status === 200 && res.data !== undefined){
+                setCategories(res.data);
             }
         });
     }
 
-    const fetchCategoryData = (category) => {
-        if(category === 'Category'){
-            fetchData();
-        }
-        fetchDataByCategory(0, recordsPerPage, category).then((res) => {
-            if(res.status === 200 && res.data !== undefined && res.data.products !== undefined){
-                setData(res.data.products);
-                setTotalPages(res.data.total/recordsPerPage);
-                setCurrentPage(res.data.skip/recordsPerPage+1);
-            }
-        });
+    const filterByCategory = (event) => {
+        const category = event.target.value;
+        // categoryFilter(category);
+        categoryHandler(category);
     }
 
     useEffect(() => {
         fetchData();
-      }, [currentPage]);
-
-    const updateCheckedRows =(id) => {
-        const updatedRows = selectedRows.includes(id) 
-                            ? selectedRows.filter((rowId) => rowId !== id) 
-                            : [...selectedRows, id];
-
-        setSelectedRows(updatedRows);
-        onDataSelected(updatedRows);
-    }
+    }, []);
 
     return(
-        <div style={{paddingLeft: '10px'}}>
-            <Row>
-            <Table striped bordered hover responsive>
-            <thead>
-                <Header categoryFilter={fetchCategoryData}/>
-            </thead>
-            <tbody>
-            {(data !== undefined) && data.map((row) => (
-                    <RowData key={row.id} props={row} checked={selectedRows.includes(row.id)} checkHandler={ () => updateCheckedRows(row.id)} />
-                )) }
-            </tbody>
-            </Table>
-            </Row>
-            <Row className="float-end">
-            {(data !== undefined) && (
-                <PaginationComponent currentPage={currentPage} setCurrentPage={setCurrentPage} totalPages={totalPages} />
-            )}
-            </Row>
-        </div>
+        <tr>
+            <th>###</th>
+            <th>Title</th>
+            <th>Brand</th>
+            <th>
+            <Form.Select aria-label="Default select example" onChange={filterByCategory}>
+                <option>Category</option>
+                {(categories !== undefined) && (categories.map((category) => (
+                                <option key={category} value={category} >{category}</option>
+                            )))}
+            </Form.Select>
+            </th>
+            <th>Thumbnail</th>
+        </tr>
     );
 }
-
-export default TableData;
